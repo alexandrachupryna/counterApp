53ef7afbf986c75564f425703691492b35bf35b3
alexandrachupryna <alexandrachuprina93@gmail.com>
2019-07-26 21:51:12 +0300

    Updates

---
+++ precache-manifest.de8399e8bd122320559a782873ec91fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e468c1505d170391d0c74f7f5396d3",
+    "url": "/counterApp/index.html"
+  },
+  {
+    "revision": "f220ab97d104c777e7ad",
+    "url": "/counterApp/static/css/main.9b18833a.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/counterApp/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "f220ab97d104c777e7ad",
+    "url": "/counterApp/static/js/main.2dfdef55.chunk.js"
+  },
+  {
+    "revision": "a2a8382bbcd301be218f",
+    "url": "/counterApp/static/js/runtime~main.5657e1cb.js"
+  }
+]);
